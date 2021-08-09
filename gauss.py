766d50b717f81ceb71af2269f59d4aed4b143c4e
ofinke <65545889+ofinke@users.noreply.github.com>
2021-08-09 18:59:01 +0200

    2021-08-09-A
    Updated the network so it manages to learn even if the  gaussian function is randomly shifted by some value

--- gauss.py
+++ gauss.py
@@ -2,9 +2,9 @@
 
 import numpy as np
 
-def gauss(x, sigma, noise=False):
+def gauss(x, sigma, xshift=0, noise=False):
     # normalized gaussian density function
-    g = (1/(sigma*np.sqrt(2*np.pi)))*np.exp((-1/2)*((x/sigma)**2))
+    g = (1/(sigma*np.sqrt(2*np.pi)))*np.exp((-1/2)*(((x+xshift)/sigma)**2))
     if noise is True:
         noise = np.random.normal(0, 0.005, len(x))
         g = g + noise
