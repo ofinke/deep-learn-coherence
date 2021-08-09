d556839bd177b027c29d9b1ab40a4ac02803b166
ofinke <ondrej.finke@gmail.com>
2021-08-09 16:50:42 +0200

    2021-08-09
    Finished first instance of NN for predicting sigma of 1D gaussian function

--- gauss.py
+++ gauss.py
@@ -5,7 +5,6 @@ import numpy as np
 def gauss(x, sigma, noise=False):
     # normalized gaussian density function
     g = (1/(sigma*np.sqrt(2*np.pi)))*np.exp((-1/2)*((x/sigma)**2))
-
     if noise is True:
         noise = np.random.normal(0, 0.005, len(x))
         g = g + noise
